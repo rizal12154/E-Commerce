4c7cacd977e7688757ddeadb7fd18bf7ad40b660
Rizal <rizalvanderoz12154@gmail.com>
2024-12-26 18:34:01 +0700

    Update

--- resources/views/layouts/script.blade.php
+++
@@ -1,4 +0,0 @@
-<!-- all js -->
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script> --}}
-<script src="assets/js/vendor.js"></script>
-<script src="assets/js/main.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\CheckoutController;
-use App\Http\Controllers\DetailController;
-use App\Http\Controllers\ErrorController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\KeranjangController;
-use App\Http\Controllers\ProdukController;
-use App\Http\Controllers\WhitelistController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', function () {
-//     return view('layouts/dashboard');
-// });
-
-Route::get('/home', [HomeController::class, 'index']);
-
-Route::get('/produk', [ProdukController::class, 'index']);
-
-Route::get('/keranjang', [KeranjangController::class, 'index']);
-
-Route::get('/checkout', [CheckoutController::class, 'index']);
-
-Route::get('/contact', [ContactController::class, 'index']);
-
-Route::get('/whitelist', [WhitelistController::class, 'index']);
-
-Route::get('/eror', [ErrorController::class, 'index']);
-
-Route::get('/detail', [DetailController::class, 'index']);
